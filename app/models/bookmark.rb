b9db229f1e08daf9d1efee1b0be41248414953d1
Til Lombardo <til.lombardo@hotmail.fr>
2023-02-26 13:21:00 +0100

    controllers views -error with bookmark add

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, uniqueness: { scope: :list_id, message: 'has already been bookmarked' }
-  validates :comment, length: { minimum: 6, message: 'your comment should have at least 6 characters' }
+  validates :comment, length: {minimum: 6}
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
+require 'open-uri'
+require  'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+puts 'Cleaning DB...'
+Movie.destroy_all
+puts 'DB cleaned!'
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+reader = URI.open(url).read
+result = JSON.parse(reader)['results']
+result.each do |movie|
+  puts 'Creating movie...'
+  base_url_poster = 'https://image.tmdb.org/t/p/original'
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    rating: movie['vote_average'],
+    poster_url: "#{base_url_poster}#{movie['poster_path']}"
+  )
+end
+puts 'Movies created!'
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
